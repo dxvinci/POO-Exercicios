5de5501cc87e6653a8d4d5931ac5f453b949e37d
Davi Ribeiro <davirpa1@gmail.com>
2023-08-29 13:47:28 -0300

    :hammer: refactor: classe App

--- Exercicios/Lista02-Basicos/Exercicio-3/App.java
+++ Exercicios/Lista02-Basicos/Exercicio-3/App.java
@@ -2,9 +2,21 @@ import java.util.Scanner;
 
 public class App {
 
+    public static Partida criarPartida() {
+        Scanner sc = new Scanner(System.in);
+        String equipe1, equipe2;
+
+        System.out.println("Escreva os nomes das equipes: ");
+        equipe1 = sc.nextLine();
+        equipe2 = sc.nextLine();
+
+        return new Partida(equipe1, equipe2);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Partida partida = new Partida("Vasco", "Flamengo");
+
+        Partida partida = criarPartida();
 
         while (partida.continua()) {
             partida.cadastrarPeriodo(sc.nextInt(), sc.nextInt());
